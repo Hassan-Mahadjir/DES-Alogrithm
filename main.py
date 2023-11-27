2cf429f6d54e6cec2b9d6432378736c52799d4e5
Hassan-Mahadjir <133597308+Hassan-Mahadjir@users.noreply.github.com>
2023-11-27 19:21:08 +0200

    test-binary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from convertTobits import converToBinary
 from permutation import permutation, initialPermutationMatrix
-plain_text = "helloooo"
+plain_text = "helloooohelloooohelloooohelloooo"
 key = "hassan"
 
 # while len(key) != 8:
@@ -9,9 +9,52 @@ key = "hassan"
 
 plain_text_bits = converToBinary(plain_text)
 key_bits = converToBinary(key)
-print([bit for bit in plain_text_bits])
-print()
+
+isPaddingRequired = (len(plain_text_bits) % 8 != 0)
+
+print([int(bit) for bit in plain_text_bits])
+# print()
+
 block = permutation(plain_text_bits, initialPermutationMatrix)
-print(block)
-print(len(block))
-print(len(plain_text_bits))
+# print(block)
+
+
+def binValue(val, bitSize):
+    """Function to return the binary value as a string of given size."""
+
+    binVal = bin(val)[2:] if isinstance(val, int) else bin(ord(val))[2:]
+
+    # Appending with required number of zeros in front
+    while len(binVal) < bitSize:
+        binVal = "0" + binVal
+
+    # Returning binary value
+    return binVal
+
+
+def stringToBitArray(text):
+    """Funtion to convert a string into a list of bits."""
+
+    # Initializing variable required
+    bitArray = []
+    for letter in text:
+        # Getting binary (8-bit) value of letter
+        binVal = binValue(letter, 8)
+        # Making list of the bits
+        binValArr = [int(x) for x in list(binVal)]
+        # Apending the bits to array
+        bitArray += binValArr
+
+    # Returning answer
+    return bitArray
+
+
+function_array = stringToBitArray(plain_text)
+print(function_array)
+
+
+for i in range(len(function_array)):
+    if function_array[i] != int(plain_text_bits[i]):
+        print(f"false {i}")
+    else:
+        print(True)
