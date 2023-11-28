fb5eb3f5f011d8dbc8040e0bd525e9e9fd69c50c
Hassan-Mahadjir <133597308+Hassan-Mahadjir@users.noreply.github.com>
2023-11-28 19:30:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from Encryption import DESEncryption
 from decryption import DESDecryption
 
-plain_text = input('Enter text: ')
+text = input('Enter text: ')
 key = input("Enter a key (ony 8 bytes)")
+condition = input("select Encryption(e) or Decryption(d)")
 while len(key) != 8:
     key = input("Enter a key (ony 8 bytes)")
-isExtensionRequired = (len(plain_text) % 8 != 0)
+isExtensionRequired = (len(text) % 8 != 0)
 
-result_of_encryption = DESEncryption(
-    text=plain_text, key=key, extension=isExtensionRequired)
+if condition == 'e':
+    result_of_encryption = DESEncryption(
+        text=text, key=key, extension=isExtensionRequired)
+    print(result_of_encryption)
 
-print(result_of_encryption)
-
-result_of_decryption = DESDecryption(
-    key=key, text=result_of_encryption, extension=isExtensionRequired)
-
-print(result_of_decryption)
+elif condition == 'd':
+    result_of_decryption = DESDecryption(
+        key=key, text=text, extension=isExtensionRequired)
+    print(result_of_decryption)
