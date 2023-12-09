16882de6d274f7a9523cf5ba64583f260c7295c1
Hassan-Mahadjir <133597308+Hassan-Mahadjir@users.noreply.github.com>
2023-12-09 15:06:47 +0200

    Final version of DES

--- main.py
+++ main.py
@@ -1,55 +1,9 @@
-import socket
-from Encryption import DESEncryption
-from decryption import DESDecryption
+from server import receiver, sender
 
-input_text = input('Enter text:')
-key = input("Enter a key (ony 8 bytes):")
-condition = input("select Encryption(e) or Decryption(d): ")
+choose = input("Do you want to send(s) or receive(r) massage: ")
 
-while len(key) != 8:
-    key = input("Enter a key (ony 8 bytes)")
+if choose == "s":
+    sender()
 
-isExtensionRequired = (len(input_text) % 8 != 0)
-
-
-if condition == 'e':
-    result_of_encryption = DESEncryption(
-        text=input_text, key=key, extension=isExtensionRequired)
-
-    print("Encrypted Ciphertext is : %r " % result_of_encryption)
-    result_of_encryption = "%r " % result_of_encryption
-    print(result_of_encryption)
-
-elif condition == 'd':
-    result_of_decryption = DESDecryption(
-        key=key, text=input_text, extension=isExtensionRequired)
-
-    print(result_of_decryption)
-
-# from Encryption import DESEncryption
-# from decryption import DESDecryption
-
-
-# condition = input("select Encryption(e) or Decryption(d):")
-# key = input("Enter a key (ony 8 bytes):")
-# while len(key) != 8:
-#     key = input("Enter a key (ony 8 bytes)")
-
-
-# if condition == 'e':
-#     text = input('Enter text: ')
-#     isExtensionRequired = (len(text) % 8 != 0)
-#     f = open("demofile2.txt", "w")
-#     result_of_encryption = DESEncryption(
-#         text=text, key=key, extension=isExtensionRequired)
-#     f.write(result_of_encryption)
-#     print(result_of_encryption)
-#     f.close()
-
-# elif condition == 'd':
-#     read = open("demofile2.txt", "r")
-#     read1 = read.read()
-#     result_of_decryption = DESDecryption(
-#         key=key, text=read1, extension=False)
-#     print(result_of_decryption)
-#     read.close()
+elif choose == "r":
+    receiver()
--- server.py
+++ server.py
@@ -1,33 +1,67 @@
 from decryption import DESDecryption
-
+from Encryption import DESEncryption
 import socket
 
-# Set the host and port
-host = '127.0.0.1'
-port = 12345
+import re
 
-# Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to a specific address and port
-server_socket.bind((host, port))
+def remove_unwanted_characters(input_string):
+    # Define a regular expression pattern to match unwanted characters
+    pattern = re.compile(r'[\x00-\x1F\x7F-\x9F]')
 
-# Listen for incoming connections
-server_socket.listen()
+    # Use the sub method to replace matched characters with an empty string
+    cleaned_string = pattern.sub('', input_string)
 
-print(f"Server listening on {host}:{port}")
+    return cleaned_string
 
-# Accept a connection from a client
-client_socket, addr = server_socket.accept()
-print(f"Connection from {addr}")
 
-# Receive data from the client
-data = client_socket.recv(1024)
-isExtensionRequired = (len(data) % 8 != 0)
+def receiver():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('', 12345))
+    server.listen()
 
-result_of_decryption = DESDecryption(
-    key='12345678', text=data, extension=isExtensionRequired)
-print(f"Received data: {data.decode('utf-8')}")
+    client, addr = server.accept()
 
-# Close the connection
-client_socket.close()
+    print(f"Connection from {addr} established")
+    print("#massage received#")
+
+    msg = client.recv(1024)
+    encrypt = msg.decode("utf-8")
+    print("Encrypted Ciphertext is : %r " % encrypt)
+
+    key = input("Enter the key (ony 8 bytes):")
+    while len(key) != 8:
+        key = input("Enter the key (ony 8 bytes)")
+
+    isExtensionRequired = (len(encrypt) % 8 != 0)
+
+    result_of_decryption = DESDecryption(
+        key=key, text=encrypt, extension=isExtensionRequired)
+
+    print("Decrypted text is : ",
+          remove_unwanted_characters(result_of_decryption))
+    # print("Plain_text: ", result_of_decryption)
+
+    client.close()
+    server.close()
+
+
+def sender():
+    ip = input("enter ip address of the receiver: ")
+
+    # get the key and plain_text from user and encrypt it
+    input_text = input('Enter text:')
+    key = input("Enter a key (ony 8 bytes):")
+    while len(key) != 8:
+        key = input("Enter a key (ony 8 bytes)")
+    isExtensionRequired = (len(input_text) % 8 != 0)
+    result_of_encryption = DESEncryption(
+        text=input_text, key=key, extension=isExtensionRequired)
+
+    print("Encrypted Ciphertext is : %r " % result_of_encryption)
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((ip, 12345))
+
+    client.send(bytes(result_of_encryption, "utf-8"))
+    client.close()
